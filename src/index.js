04340f16330aa0796c1f48ae40cd4304afc7c83e
Jjae <143724847+guesswhojustme@users.noreply.github.com>
2026-03-25 16:40:31 +0800

    define files and structure of app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+const contentDiv = document.getElementById('content');
+
+console.log("hello mama");
+
+const restaurantDesc = document.createElement('h2')
+restaurantDesc.textContent = "Welcome to my Nonsense Restaurant! Where we cook absolute garbage food!"
+
+contentDiv.append(restaurantDesc)
